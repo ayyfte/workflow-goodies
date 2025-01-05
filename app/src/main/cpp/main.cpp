b9f1aacffdd8c6bd8e819594b11f1c8f92cde545
whmsft <whmsft@outlook.com>
2025-01-05 19:54:51 +0530

    update to latest commit

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <cmath>
 #include "include/raylib.h"
 #include "include/nlohmann/json.hpp"
 using namespace std;
@@ -15,18 +16,22 @@ const nlohmann::json allLevelMap = nlohmann::json::parse(allLevelMapFile);
 vector<vector<int>> tileMap;
 int level = 0;
 int satisfiedPlaceholders = 0;
-const int maxLevel = 3;
+const int maxLevel = 10;
 int screenWidth, screenHeight, offsetX, offsetY, tileSize;
 int levelWidth, levelHeight;
-int frame = 0;
+int FRAME = 0;
 int playerMapX, playerMapY, playerX, playerY;
 vector<vector<int>> box, boxMap;
 vector<vector<int>> placeholder, placeholderMap;
-int attachedboxIndex = -1;
-bool inputAllowed=true;
+int attachedBoxIndex = -1;
+int SCENE = 0;
+bool nextLevel = false;
+bool playerGoingDown = true;
+int sceneZeroYScroll = 0;
+Vector2 lastMousePosition = { 0.0f, 0.0f };
 
-Image imageCornerTopLeft, imageCornerTopRight, imageCornerBottomLeft, imageCornerBottomRight, imageWallUp, imageWallDown, imageWallLeft, imageWallRight, imagePlayer, imagePurpleBox, imagePurplePlaceholder, imageGreenBox, imageGreenPlaceholder;
-Texture2D textureCornerTopLeft, textureCornerTopRight, textureCornerBottomLeft, textureCornerBottomRight, textureWallUp, textureWallDown, textureWallLeft, textureWallRight, texturePlayer, texturePurpleBox, texturePurplePlaceholder, textureGreenBox, textureGreenPlaceholder;
+Image imageCornerTopLeft, imageCornerTopRight, imageCornerBottomLeft, imageCornerBottomRight, imageWallUp, imageWallDown, imageWallLeft, imageWallRight, imagePlayer, imagePurpleBox, imagePurplePlaceholder, imageGreenBox, imageGreenPlaceholder, imageBlueBox, imageBluePlaceholder, imageRedBox, imageRedPlaceholder;
+Texture2D textureCornerTopLeft, textureCornerTopRight, textureCornerBottomLeft, textureCornerBottomRight, textureWallUp, textureWallDown, textureWallLeft, textureWallRight, texturePlayer, texturePurpleBox, texturePurplePlaceholder, textureGreenBox, textureGreenPlaceholder, textureBlueBox, textureBluePlaceholder, textureRedBox, textureRedPlaceholder;
 
 void LoadTextures() {
     UnloadImage(imageCornerTopLeft);
@@ -42,6 +47,10 @@ void LoadTextures() {
     UnloadImage(imagePurplePlaceholder);
     UnloadImage(imageGreenBox);
     UnloadImage(imageGreenPlaceholder);
+    UnloadImage(imageBlueBox);
+    UnloadImage(imageBluePlaceholder);
+    UnloadImage(imageRedBox);
+    UnloadImage(imageRedPlaceholder);
     imageCornerTopLeft = LoadImage("assets/corner_topleft.png");
     imageCornerTopRight = LoadImage("assets/corner_topright.png");
     imageCornerBottomLeft = LoadImage("assets/corner_bottomleft.png");
@@ -53,8 +62,12 @@ void LoadTextures() {
     imagePlayer = LoadImage("assets/player.png");
     imagePurpleBox = LoadImage("assets/purple_box.png");
     imagePurplePlaceholder = LoadImage("assets/purple_placeholder.png");
-    imageGreenBox = LoadImage("assets/purple_box.png");
-    imageGreenPlaceholder = LoadImage("assets/purple_placeholder.png");
+    imageGreenBox = LoadImage("assets/green_box.png");
+    imageGreenPlaceholder = LoadImage("assets/green_placeholder.png");
+    imageBlueBox = LoadImage("assets/blue_box.png");
+    imageBluePlaceholder = LoadImage("assets/blue_placeholder.png");
+    imageRedBox = LoadImage("assets/red_box.png");
+    imageRedPlaceholder = LoadImage("assets/red_placeholder.png");
     ImageResizeNN(&imageCornerTopLeft, tileSize, tileSize);
     ImageResizeNN(&imageCornerTopRight, tileSize, tileSize);
     ImageResizeNN(&imageCornerBottomLeft, tileSize, tileSize);
@@ -68,6 +81,10 @@ void LoadTextures() {
     ImageResizeNN(&imagePurplePlaceholder, tileSize, tileSize);
     ImageResizeNN(&imageGreenBox, tileSize, tileSize);
     ImageResizeNN(&imageGreenPlaceholder, tileSize, tileSize);
+    ImageResizeNN(&imageBlueBox, tileSize, tileSize);
+    ImageResizeNN(&imageBluePlaceholder, tileSize, tileSize);
+    ImageResizeNN(&imageRedBox, tileSize, tileSize);
+    ImageResizeNN(&imageRedPlaceholder, tileSize, tileSize);
     UnloadTexture(textureCornerTopLeft);
     UnloadTexture(textureCornerTopRight);
     UnloadTexture(textureCornerBottomLeft);
@@ -81,6 +98,10 @@ void LoadTextures() {
     UnloadTexture(texturePurplePlaceholder);
     UnloadTexture(textureGreenBox);
     UnloadTexture(textureGreenPlaceholder);
+    UnloadTexture(textureBlueBox);
+    UnloadTexture(textureBluePlaceholder);
+    UnloadTexture(textureRedBox);
+    UnloadTexture(textureRedPlaceholder);
     textureCornerTopLeft = LoadTextureFromImage(imageCornerTopLeft);
     textureCornerTopRight = LoadTextureFromImage(imageCornerTopRight);
     textureCornerBottomLeft = LoadTextureFromImage(imageCornerBottomLeft);
@@ -94,6 +115,10 @@ void LoadTextures() {
     texturePurplePlaceholder = LoadTextureFromImage(imagePurplePlaceholder);
     textureGreenBox = LoadTextureFromImage(imageGreenBox);
     textureGreenPlaceholder = LoadTextureFromImage(imageGreenPlaceholder);
+    textureBlueBox = LoadTextureFromImage(imageBlueBox);
+    textureBluePlaceholder = LoadTextureFromImage(imageBluePlaceholder);
+    textureRedBox = LoadTextureFromImage(imageRedBox);
+    textureRedPlaceholder = LoadTextureFromImage(imageRedPlaceholder);
 }
 
 int newDropMapY(int positionX, int positionY) {
@@ -106,9 +131,10 @@ int newDropMapY(int positionX, int positionY) {
 void InitLevel() {
     if (level==maxLevel) level = 0;
     level+=1;
+
     levelWidth = allLevelMap[to_string(level)]["width"].get<int>();
     levelHeight = allLevelMap[to_string(level)]["height"].get<int>();
-    
+
     tileMap.resize(levelHeight);
     for (int i = 0; i < levelHeight; ++i) {
         tileMap[i].resize(levelWidth);
@@ -116,11 +142,12 @@ void InitLevel() {
             tileMap[i][j] = allLevelMap[to_string(level)]["tilemap"][i][j].get<int>();
         }
     }
-
-    screenWidth = GetScreenWidth();
-    screenHeight = GetScreenHeight();
-
-    tileSize = screenWidth / (levelWidth + 2);
+    
+    if (screenHeight>screenWidth) {
+        tileSize = screenWidth / (levelWidth + 2);
+    } else {
+        tileSize = screenHeight / (levelHeight + 2);
+    }
     offsetX = (screenWidth - (levelWidth * tileSize)) / 2;
     offsetY = (screenHeight - (levelHeight * tileSize)) / 2;
 
@@ -166,8 +193,10 @@ int main(void) {
     emscripten_set_main_loop(gameLoop, 30, 1);
     #else
     InitWindow(320, 640, "abductix");
+    //InitWindow(GetMonitorWidth(GetCurrentMonitor()), GetMonitorHeight(GetCurrentMonitor()), "outsideIn");
+    //ToggleFullscreen();
     SetTargetFPS(30);
-    InitLevel();
+    //InitLevel();
     
     while (!WindowShouldClose()) gameLoop();
     #endif
@@ -185,37 +214,52 @@ int main(void) {
 }
 
 void update() {
-    if (IsWindowResized()) {
-        screenWidth = GetScreenWidth();
-        screenHeight = GetScreenHeight();
-        LoadTextures();
+    screenWidth = GetScreenWidth();
+    screenHeight = GetScreenHeight();
+    if (SCENE==0) {
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) sceneZeroYScroll += GetMousePosition().y - lastMousePosition.y;
+    if (sceneZeroYScroll>0) sceneZeroYScroll-=(screenHeight/60);
+    if (-sceneZeroYScroll>screenWidth/16+((maxLevel-9)/3)*screenWidth/4+(screenWidth/16)*((maxLevel-9)/3)+screenWidth/4) sceneZeroYScroll+=screenHeight/60;
+    if (abs(sceneZeroYScroll-(screenHeight/60))<screenHeight/60) sceneZeroYScroll=0;
+    if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && GetMousePosition().y-lastMousePosition.y==0) {level=0;InitLevel();SCENE=1;}
     }
+    if (SCENE==1) {
+    FRAME+=1;
+    playerY += (playerGoingDown) ? +tileSize/60:-tileSize/60;
+    if (playerY>=offsetY+playerMapY*tileSize+tileSize/12) playerGoingDown=false;
+    if (playerY<=offsetY+playerMapY*tileSize-tileSize/12) playerGoingDown=true;
+    if (nextLevel) {InitLevel(); nextLevel=false;}
+    bool inputCheck = true;
     bool dragging = false;
-    if ((IsKeyPressed(KEY_RIGHT) || GetGestureDetected()==GESTURE_SWIPE_RIGHT) && playerMapX+1<levelWidth && inputAllowed) {playerMapX++;dragging=true;}
-    if ((IsKeyPressed(KEY_LEFT) || GetGestureDetected()==GESTURE_SWIPE_LEFT) && playerMapX>0 && inputAllowed) {playerMapX--;dragging=true;}
+    if ((IsKeyPressed(KEY_RIGHT) || GetGestureDetected()==GESTURE_SWIPE_RIGHT) && playerMapX+1<levelWidth) {playerMapX++;dragging=true;}
+    if ((IsKeyPressed(KEY_LEFT) || GetGestureDetected()==GESTURE_SWIPE_LEFT) && playerMapX>0) {playerMapX--;dragging=true;}
 
-    if (abs(playerX-(offsetX+playerMapX*tileSize)) < tileSize/4) {playerX = offsetX+playerMapX*tileSize; inputAllowed=true;}
-    if (abs(playerY-(offsetY+playerMapX*tileSize)) < tileSize/4) {playerY = offsetY+playerMapY*tileSize; inputAllowed=true;}
-    if (playerX < offsetX + playerMapX * tileSize) {playerX += tileSize / 4; inputAllowed=false;}
-    if (playerX > offsetX + playerMapX * tileSize) {playerX -= tileSize / 4; inputAllowed=false;}
+    if (abs(playerX-(offsetX+playerMapX*tileSize)) < tileSize/4) playerX = offsetX+playerMapX*tileSize;
+    if (playerX < offsetX + playerMapX * tileSize) playerX += tileSize / 4;
+    if (playerX > offsetX + playerMapX * tileSize) playerX -= tileSize / 4;
     satisfiedPlaceholders = 0;
-    int indexToLift = -1;
+    int indexToLift = -1;    
     for (int i = 0; i < boxMap.size(); ++i) {
-        if (attachedboxIndex!=i) tileMap[boxMap[i][1]][boxMap[i][0]] = 10+i;
-        if (box[i][1]<=playerY+tileSize+1) attachedboxIndex = i;
-        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && attachedboxIndex==-1 && (playerMapX == boxMap[i][0]) && inputAllowed && !dragging) {
-            if (indexToLift == -1) {indexToLift = i;} else if (boxMap[indexToLift][1]>boxMap[i][1]) {indexToLift=i;}
+        if (attachedBoxIndex!=i) tileMap[boxMap[i][1]][boxMap[i][0]] = 10+i;
+        if (box[i][1]<=playerY+tileSize+1) attachedBoxIndex = i;
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && attachedBoxIndex==-1 && (playerMapX == boxMap[i][0]) && !dragging && inputCheck) {
+            if (indexToLift == -1 or box[indexToLift][1]>box[i][1]) indexToLift=i;
         }
-        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && attachedboxIndex==i && inputAllowed && !dragging) {
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && attachedBoxIndex==i && !dragging && indexToLift!=i && inputCheck && newDropMapY(boxMap[i][0], boxMap[i][1])!=-1) {
             tileMap[boxMap[i][1]][boxMap[i][0]] = 0;
-            attachedboxIndex=-1;
+            attachedBoxIndex=-1;
             boxMap[i][1] = newDropMapY(boxMap[i][0], boxMap[i][1]);
+            inputCheck = false;
         }
-        if (abs(box[i][0]-(offsetX+boxMap[i][0]*tileSize)) <= tileSize/4) {box[i][0] = offsetX+boxMap[i][0]*tileSize; inputAllowed=true;}
-        if (abs(box[i][1]-(offsetY+boxMap[i][1]*tileSize)) <= tileSize/4) {box[i][1] = offsetY+boxMap[i][1]*tileSize; inputAllowed=true;}
-        if (box[i][1] < offsetY + boxMap[i][1] * tileSize) {box[i][1] += tileSize / 4; inputAllowed=false;}
-        if (box[i][1] > offsetY + boxMap[i][1] * tileSize) {box[i][1] -= tileSize / 4; inputAllowed=false;}
-        if (playerMapY+1==boxMap[i][1] && attachedboxIndex==i) {boxMap[i][0]=playerMapX;box[i][0]=playerX;}
+        if (abs(box[i][1]-(offsetY+boxMap[i][1]*tileSize)) <= tileSize/4) box[i][1] = offsetY+boxMap[i][1]*tileSize;
+        if (box[i][1] < offsetY + boxMap[i][1] * tileSize) box[i][1] += tileSize / 4;
+        if (box[i][1] > offsetY + boxMap[i][1] * tileSize) box[i][1] -= tileSize / 4;
+        if (attachedBoxIndex==i) {box[i][1] = playerY+tileSize;} else {
+            if (box[i][0] < offsetX + boxMap[i][0] * tileSize) box[i][0] += tileSize / 4;
+            if (box[i][0] > offsetX + boxMap[i][0] * tileSize) box[i][0] -= tileSize / 4;
+            if (abs(box[i][0]-(offsetX+boxMap[i][0]*tileSize)) <= tileSize/4) box[i][0] = offsetX+boxMap[i][0]*tileSize;
+        }
+        if (playerMapY+1==boxMap[i][1]) {boxMap[i][0]=playerMapX;box[i][0]=playerX;}
         for (int j=0; j<placeholder.size(); ++j) {
             if (placeholder[j][0] == box[i][0] && placeholder[j][1] == box[i][1] && placeholderMap[j][2]==boxMap[i][2]) ++satisfiedPlaceholders;
         }
@@ -224,11 +268,23 @@ void update() {
         tileMap[boxMap[indexToLift][1]][boxMap[indexToLift][0]] = 0;
         boxMap[indexToLift][1] = playerMapY+1;
     }
-    if (satisfiedPlaceholders==placeholderMap.size()) InitLevel();
+    if (satisfiedPlaceholders==placeholderMap.size()) nextLevel = true;
+    }
+    lastMousePosition = GetMousePosition();
 }
-
 void draw() {
     ClearBackground(ColorFromHSV(302, .54, .52));
+    if (SCENE==0) {
+        int width = screenWidth/4;
+        DrawText("abductix",screenWidth/2-MeasureText("abductix",screenWidth/5)/2,sceneZeroYScroll+screenHeight/4,screenWidth/5,WHITE);
+        for (int i = 0; i < maxLevel; i++) {
+            int x = (i % 3) * width + (width/4)*(i%3);
+            int y = (i / 3) * width + (width/4)*(i/3);
+            DrawRectangle(width/4+x, sceneZeroYScroll+screenHeight/2+width/4+y, width, width, WHITE);
+        }
+        DrawRectangleGradientV(0, screenHeight-screenHeight/8, screenWidth, screenHeight/8, (Color){255, 255, 255, 0}, (Color){255, 255, 255, 128});
+    }
+    else if (SCENE==1) {
     for (nlohmann::json::size_type rowIndex = 0; rowIndex < tileMap.size(); ++rowIndex) {
         const auto& row = tileMap[rowIndex];
         for (nlohmann::json::size_type tileIndex = 0; tileIndex < row.size(); ++tileIndex) {
@@ -252,12 +308,20 @@ void draw() {
 
     DrawTexture(texturePlayer, playerX, playerY, WHITE);
     for (int i = 0; i < placeholderMap.size(); ++i) {
-        if (placeholderMap[i][2]==0) {DrawTexture(texturePurplePlaceholder, placeholder[i][0], placeholder[i][1], WHITE);}
-        else if (placeholderMap[i][2]==1) {DrawTexture(textureGreenPlaceholder, placeholder[i][0], placeholder[i][1], WHITE);}
+        switch (placeholderMap[i][2]) {
+            case 0: DrawTexture(texturePurplePlaceholder, placeholder[i][0], placeholder[i][1], WHITE); break;
+            case 1: DrawTexture(textureGreenPlaceholder, placeholder[i][0], placeholder[i][1], WHITE); break;
+            case 2: DrawTexture(textureBluePlaceholder, placeholder[i][0], placeholder[i][1], WHITE); break;
+            case 3: DrawTexture(textureRedPlaceholder, placeholder[i][0], placeholder[i][1], WHITE); break;
+        }
     }
     for (int i = 0; i < boxMap.size(); ++i) {
-        if (boxMap[i][2]==0) {DrawTexture(texturePurpleBox, box[i][0], box[i][1], WHITE);}
-        else if (boxMap[i][2]==1) {DrawTexture(textureGreenBox, box[i][0], box[i][1], WHITE);}
+        switch (boxMap[i][2]) {
+            case 0: DrawTexture(texturePurpleBox, box[i][0], box[i][1], WHITE); break;
+            case 1: DrawTexture(textureGreenBox, box[i][0], box[i][1], WHITE); break;
+            case 2: DrawTexture(textureBlueBox, box[i][0], box[i][1], WHITE); break;
+            case 3: DrawTexture(textureRedBox, box[i][0], box[i][1], WHITE); break;
+        }
     }
-    // DrawText(TextFormat("attachedboxIndex: %i", attachedboxIndex),10,10,20,WHITE);
+    }
 }
